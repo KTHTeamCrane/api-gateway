5434871b3f95cea512d4dc9e52b0110d8f5c46d6
kristianleoruth <kristianleoruth@gmail.com>
2024-04-17 08:40:56 +0200

    Working extractor, only Node script

--- extract.js
+++ extract.js
@@ -1,6 +1,18 @@
 const extractor = require("article-extractor")
 const entities = require("entities")
 
+/*
+Function: extract article contents and `onSuccess` is called with the data JSON object as argument
+            Structure of JSON:
+            {
+              domain: String,
+              author: String,
+              title: String,
+              content: String,
+              summary: String
+            }
+Return type: none
+*/
 module.exports = {
   ExtractArticle: async function(url, onSuccess) {
     extractor.extractData(url, (err, dat) => {
