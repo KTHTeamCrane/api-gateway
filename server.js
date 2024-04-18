9a1b8e2c46edfbf2f7103147ef9bff4ff3ab7d31
ohshitnotgood <98031796+ohshitnotgood@users.noreply.github.com>
2024-04-18 10:14:23 +0200

    Added echo route and env file test routes

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ console.log(apiDomain)
 function Sanitize(str) { return str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"").trim(); }
 
 app.get("/", async (req, res) => {
+    console.log("To fake or not to fake, that is the question")
     res.send("To fake or not to fake, that is the question")
 })
 
@@ -19,30 +20,27 @@ app.get("/article", async (req, res) => {
     let articleUrl = req.query.article
 
     Sanitize(articleUrl)
-    // TODO: Send request as a body instead of a parameter
     const  func = async (_, dat) => {
         const result = await fetch(apiDomain, {
             method: "POST",
             headers: {
                 "Content-Type":"text/plain"
-            }
+            },
             body: dat.content,
             port: port
         })
         console.log(result)
-        // let result = await fetch(apiDomain + "/api/fake?article=" +  dat.content, {method: "POST"}) 
-        // console.log(result)
         res.send(JSON.stringify(await result.json())) 
     }
     console.log(articleUrl)
     extractor.ExtractArticle(articleUrl, func)
 })
 
-app.get("/echo", async (req, res) => {
-    res.send(req.body)
+app.post("/echo", async (req, res) => {
+    res.send(req.query.echo)
 })
 
-app.get("/env-test", async (res, req) => {
+app.get("/env-test", async (req, res) => {
     res.send(process.env.FLASK_API_DOMAIN)
 })
 
