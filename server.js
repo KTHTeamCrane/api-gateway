4e9fc6a34e76fb8a5e2a2a5ee965e8b03720df15
kristianleoruth <125584655+kristianleoruth@users.noreply.github.com>
2024-04-18 08:19:12 +0200

    Update server.js
    Sanitize input url

--- server.js
+++ server.js
@@ -8,16 +8,18 @@ const port = 8000
 
 console.log(apiDomain)
 
+function Sanitize(str) { return str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"").trim(); }
 
 app.get("/article", async (req, res) => {
     console.log("Request at /article")
     let articleUrl = req.query.article
 
+    Sanitize(articleUrl)
     // TODO: Send request as a body instead of a parameter
     const  func = async (_, dat) => {
         let result = await fetch(apiDomain + "/api/fake?article=" +  dat.content, {method: "POST"}) 
         console.log(result)
-    res.send(JSON.stringify(await result.json())) 
+        res.send(JSON.stringify(await result.json())) 
     }
     console.log(articleUrl)
     extractor.ExtractArticle(articleUrl, func)
