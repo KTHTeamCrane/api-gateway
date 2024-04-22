58c5454f33f2a742a83d620f265630e3d8b6ab32
ohshitnotgood <98031796+ohshitnotgood@users.noreply.github.com>
2024-04-22 22:26:21 +0200

    Added untested article extractor code

--- src/extract.ts
+++ src/extract.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from "express";
+import {decodeHTML} from "entities"
+import {ArticleData, extract} from "@extractus/article-extractor"
+import { throws } from "assert";
 
-export default function extract(req: Request, res: Response) {
+
+export default async function ep_extract(req: Request, res: Response) {
     try {
-
     } catch {
 
     }
 }
 
-export async function extract_articles(articleUrl: string) {
-
+export async function extract_articles(articleUrl: string, onSuccessCallback: (err: any, article: ArticleData) => void) {
+    let article = await extract(articleUrl)
+    if (article?.content != null) {
+        article.content = article.content.replace(/<[^>]*>/g, '')
+        article.content = decodeHTML(article.content)
+        onSuccessCallback(null, article)
+    }
 }
