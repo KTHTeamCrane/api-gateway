89c22ae786e29dbea86cce78a23a5ab4531ec58e
Kristiāns Vinters <kristians.vinters@giraffe360.com>
2024-04-25 21:10:13 +0200

    Added WS reconnections

--- src/api/websocket.ts
+++ src/api/websocket.ts
@@ -1,3 +1,10 @@
 import { WebSocket } from "ws";
 
-export const websocket = new WebSocket("wss://llm-proxy-server-slixmjmf2a-ez.a.run.app")
+export let websocket = new WebSocket("wss://llm-proxy-server-slixmjmf2a-ez.a.run.app");
+
+websocket.onclose = () => {
+    // Attempt reconnect
+    setTimeout(() => {
+        websocket = new WebSocket("wss://llm-proxy-server-slixmjmf2a-ez.a.run.app");
+    }, 1000);
+}
