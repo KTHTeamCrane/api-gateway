04d4cc9efd4ac7c0c8f88429f6e2a6fca167d43c
kristianleoruth <kristianleoruth@gmail.com>
2024-05-08 13:21:02 +0200

    Added fetch news API endpoint

--- src/app.ts
+++ src/app.ts
@@ -23,8 +23,9 @@ Route                       Method      Parameters
     res.send(help);
 });
 
-app.get('/fetchnews/', (req, res) => {
-    const country = req.url.replace("/fetchnews/", "").replaceAll("%20", " ")
+app.get('/fetchnews/:country', async (req, res) => {
+    // const country = req.url.replace("/fetchnews/", "").replaceAll("%20", " ")
+    const country = req.params.country;
   
     // Set cors headers
     //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1002')
@@ -33,57 +34,27 @@ app.get('/fetchnews/', (req, res) => {
   
     let date = new Date()
     let year = date.getFullYear(), month = date.getMonth() + 1, day = date.getDate()
-    // let year = 2024, month = 3, day = 1
-    if (day <= 7) {
-      if (month == 1) {
-        year -= 1
-        month = 12
-      }
-      else month -= 1
+    date.setDate(date.getDate() - 7);
     
-      day = day - 7
-      switch (month) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-          day = 31 + day
-          break;
-        case 2:
-          if (year % 4 == 0) day = 29 + day
-          else day = 28 + day
-          break;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-          day = 30 + day
-          break
-      }
-    }
-    else day -= 7
-    
-    if (day < 10) day = `0${day}`
-    if (month < 10) month = `0${month}`
-    console.log(`Since ${year}-${month}-${day}`)
+    let dayStr, monthStr, yearStr;
+    dayStr = `${day}`
+    monthStr = `${month}`
+    yearStr = `${year}`
+
+    if (day < 10) dayStr = `0${day}`
+    if (month < 10) monthStr = `0${month}`
+    console.log(`Since ${yearStr}-${monthStr}-${dayStr}`)
     console.log(country)
-    var url = 'https://newsapi.org/v2/everything?' +
+    
+    const url = 'https://newsapi.org/v2/everything?' +
             `q=${country}&` +
-            `from=${year}-${month}-${day}&sortBy=publishedAt&` +
+            `from=${yearStr}-${monthStr}-${dayStr}&sortBy=publishedAt&` +
             'apiKey=' + '2546a9a33a3e464ca8d5887df53c14d6'
     console.log(url)
-    var req = new Request(url)
-    fetch(req)
-      .then(function(response) {
-        return response.json()
-      }).then((js) => {
-        console.log(js)
-        res.write(JSON.stringify(js))
-        res.end()
-      })
+    const newsReq = new Request(url)
+    const newsRes = await fetch(newsReq)
+    const json = newsRes.json()
+    res.json(json);
 })
 
 app.post("/api/article/extract", async (req: Request, res: Response) => {
